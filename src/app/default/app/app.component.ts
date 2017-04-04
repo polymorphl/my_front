9bc475444ee7e84ef2187e31a40e7a83ae6b5554
LUC TERRACHER <lucterracher@gmail.com>
2017-04-04 23:59:13 +0200

    continue scss

--- src/app/default/app/app.component.ts
+++ src/app/default/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +10,58 @@ export class AppComponent implements OnInit {
 
   public title = 'my_front works!';
   public appName = 'My_front';
+  public minimalWidth = "900px"; // responsive css breakpoint
   public optionsToaster = {
     position: ["top", "right"],
     timeOut: 5000,
     lastOnBottom: true
   }
   private _opened: boolean = false;
+  public _layout = 0;
+  private _closeOnClickOutside: boolean = false;
+  private _modeNum: number = 0;
+
+  constructor(private _zone: NgZone) {
+    this.enableResponsive();
+  }
+
+  ngOnInit() { }
 
   private _toggleSidebar() {
     this._opened = !this._opened;
   }
 
-  constructor() { }
+  private enableResponsive(): void {
+    let mq = window.matchMedia(`screen and (min-width: ${this.minimalWidth})`);
+    // init
+    mq.matches ? this.desktopMode() : this.mobileMode();
+    // listen for change
+    mq.addListener((matchMedia) => {
+      this._zone.run(() => { // force update
+        matchMedia.matches ? this.desktopMode() : this.mobileMode();
+      });
+    });
+  }
 
-  ngOnInit() { }
+  private desktopMode(): void {
+    this._openSidebar();
+    this._modeNum = 1;
+    this._closeOnClickOutside = false;
+    this._layout = 0;
+  }
+
+  private mobileMode(): void {
+    this._closeSidebar();
+    this._modeNum = 0;
+    this._closeOnClickOutside = true;
+    this._layout = 1;
+  }
+
+  private _closeSidebar(): void {
+    this._opened = false;
+  }
+
+  private _openSidebar(): void {
+    this._opened = true;
+  }
 }
